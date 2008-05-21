99afe69ac8720519b41bd1d13dddc4921d855cff
hahmann <hahmann@45e35d04-6e4a-0410-b633-b5be0a9e6412>
2008-05-21 15:09:46 +0000

    small changes

---
+++ src/com/rapidminer/operator/learner/clustering/clusterer/FuzzyMembershipEvaluator.java
@@ -0,0 +1,140 @@
+package com.rapidminer.operator.learner.clustering.clusterer;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+import jxl.demo.Escher;
+
+import com.rapidminer.example.Attribute;
+import com.rapidminer.example.Example;
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.example.table.AttributeFactory;
+import com.rapidminer.example.table.DataRow;
+import com.rapidminer.example.table.DataRowFactory;
+import com.rapidminer.example.table.ListDataRowReader;
+import com.rapidminer.example.table.MemoryExampleTable;
+import com.rapidminer.operator.IOObject;
+import com.rapidminer.operator.OperatorDescription;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.UserError;
+import com.rapidminer.operator.generator.TargetFunction;
+import com.rapidminer.operator.learner.clustering.AbstractClusterModel;
+import com.rapidminer.operator.learner.clustering.ClusterModel;
+import com.rapidminer.operator.learner.clustering.FlatCrispClusterModel;
+import com.rapidminer.operator.learner.clustering.IdUtils;
+import com.rapidminer.parameter.ParameterType;
+import com.rapidminer.parameter.ParameterTypeBoolean;
+import com.rapidminer.parameter.ParameterTypeCategory;
+import com.rapidminer.parameter.ParameterTypeInt;
+import com.rapidminer.tools.Ontology;
+import com.rapidminer.tools.RandomGenerator;
+import com.rapidminer.tools.att.AttributeSet;
+
+public class FuzzyMembershipEvaluator extends AbstractFlatClusterer {
+
+	/** The parameter name for &quot;the fuzzifier&quot; */
+	public static final String PARAMETER_M = "m";
+	
+	/** The parameter name for &quot;should the membership values be added to the ExampleSet as well&quot; */
+	public static final String PARAMETER_ADD_LABEL = "add_label";
+	
+	private int m;
+	private int numAttributes;
+	private int numClusters;
+	private double[][] centroids;
+	private int[] clustersizes;
+	private FuzzyMembershipModel fmm;
+	
+	public FuzzyMembershipEvaluator(OperatorDescription description) {
+		super(description);
+	}
+
+	@Override
+	public ClusterModel createClusterModel(ExampleSet es)
+			throws OperatorException {
+		
+		m = getParameterAsInt(PARAMETER_M);
+		FlatCrispClusterModel fccm = this.getInput(FlatCrispClusterModel.class);
+		numClusters = fccm.getNumberOfClusters();
+		this.centroids = computeCentroids(es);
+		fmm = new FuzzyMembershipModel(fccm,centroids,clustersizes,es,m);
+		
+		if (getParameterAsBoolean(PARAMETER_ADD_LABEL)) {
+			addLabels(es);
+		}
+		
+		return fmm;
+	}
+
+		
+	
+	private void addLabels(ExampleSet es) {
+		
+		Vector<Attribute> ats = new Vector<Attribute>(centroids.length);
+		
+		for (int i = 0; i < centroids.length; i++) {
+			Attribute cluster = AttributeFactory.createAttribute("Cluster"+i, Ontology.REAL);
+			ats.add(cluster);
+			es.getExampleTable().addAttribute(cluster);
+			es.getAttributes().setSpecialAttribute(cluster, "Cluster"+i);
+		}
+		
+		for (int i = 0; i < es.size(); i++) {
+			
+			Vector<Double> temp = fmm.getMembership(es.getExample(i));
+			
+			for (int j = 0; j < temp.size(); j++) {
+				es.getExample(i).setValue(ats.get(j), temp.get(j));
+			}
+			
+			
+		}
+		
+	}
+
+	protected double[][] computeCentroids(ExampleSet es) throws OperatorException {
+		numAttributes = es.getAttributes().size();
+		this.centroids = new double[numClusters][numAttributes];
+		this.clustersizes = new int[numClusters];
+		for (int j = 0; j < numClusters; j++) {
+			int memcount = 0;
+			double[] temp = new double[numAttributes]; 
+			for (int i = 0; i < es.size(); i++) {
+				Example ex = es.getExample(i);
+					if (ex.getValue(ex.getAttributes().getCluster())==j) {
+						memcount++;
+						int m = 0;
+						for (Attribute att : ex.getAttributes()) {
+							if (!ex.getAttributes().getRole(att).isSpecial()) {
+								temp[m] = temp[m]+ex.getValue(att);
+								m++;
+							}
+						}
+					}
+			}
+			for (int k = 0; k < temp.length; k++) {
+				centroids[j][k] = temp[k]/memcount;
+				clustersizes[j] = memcount;
+			}
+			
+		}
+		return centroids;
+	}
+	
+	public List<ParameterType> getParameterTypes() {
+		List<ParameterType> types = super.getParameterTypes();
+		ParameterType type = new ParameterTypeInt(PARAMETER_M, "Fuzzifier", 2, Integer.MAX_VALUE, 2);
+		type.setExpert(false);
+		types.add(type);
+		types.add(new ParameterTypeBoolean(PARAMETER_ADD_LABEL, "should the cluster values be added as label as well", false));
+		return types;
+	}
+	
+	
+	
+}
+
+
---
+++ src/com/rapidminer/operator/learner/clustering/clusterer/FuzzyMembershipModel.java
@@ -0,0 +1,165 @@
+/*
+ *  RapidMiner
+ *
+ *  Copyright (C) 2001-2007 by Rapid-I and the contributors
+ *
+ *  Complete list of developers available at our web site:
+ *
+ *       http://rapid-i.com
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as 
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version. 
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *  USA.
+ */
+package com.rapidminer.operator.learner.clustering.clusterer;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import com.rapidminer.example.Attribute;
+import com.rapidminer.example.Example;
+import com.rapidminer.example.ExampleSet;
+import com.rapidminer.operator.learner.clustering.CentroidBasedClusterModel;
+import com.rapidminer.operator.learner.clustering.Cluster;
+import com.rapidminer.operator.learner.clustering.FlatCrispClusterModel;
+import com.rapidminer.tools.Tools;
+
+
+/**
+ * A cluster model used for the k-means clustering.
+ * 
+ * @author Michael Wurst, Ingo Mierswa
+ * @version $Id: KMeansClusterModel.java,v 1.3 2007/06/30 23:24:35 ingomierswa Exp $
+ */
+public class FuzzyMembershipModel extends FlatCrispClusterModel implements CentroidBasedClusterModel {
+
+	private static final long serialVersionUID = 3162433985759604081L;
+
+	private double[][] centroids;
+	private int[] clustersizes;
+	private ExampleSet es;
+	private FlatCrispClusterModel cm;
+	private int m;
+	private float exp;
+	private DecimalFormat df = new DecimalFormat("0.00");
+
+	public FuzzyMembershipModel(FlatCrispClusterModel cm ,double[][] centroids,int[] clustersizes, ExampleSet es, int m) {
+		// Call copy constructor
+		super();
+		// Assign centroids
+		this.centroids = centroids;
+		this.clustersizes = clustersizes;
+		this.es = es;
+		this.cm = cm;
+		this.m = m;
+		this.exp = 2/(this.m-1);
+		
+		
+		
+	}
+
+	public Vector<Double> getMembership(Example e){
+		
+			Vector<Double> result = new Vector<Double>(centroids.length);	
+			LinkedList<Double> dists = new LinkedList<Double>();
+			
+			for (int j = 0; j < centroids.length; j++) {
+				dists.add(getDistanceFromCentroid(j, e));
+			}
+			
+					double sum = 0;
+					for (int l = 0; l < centroids.length; l++) {
+						sum=0;
+						for (int k = 0; k < centroids.length; k++) {
+							sum += Math.pow(dists.get(l) / dists.get(k),exp);
+						}
+					double mem = 1/sum;
+					result.add(Math.round(mem*100.)/100.);
+					}
+			return result;		
+	}
+
+	public double getCentroidDistance(int index1, int index2) {
+		double sum = 0.0;
+		for (int i = 0; i < centroids[0].length; i++)
+			sum = sum + (centroids[index1][i] - centroids[index2][i]) * (centroids[index1][i] - centroids[index2][i]);
+		return Math.sqrt(sum);
+	}
+
+	public double getDistanceFromCentroid(int index, Example e) {
+		double sum = 0.0;
+		int i = 0;
+		for (Attribute att : e.getAttributes()) {
+			sum = sum + (centroids[index][i] - e.getValue(att)) * (centroids[index][i] - e.getValue(att));
+			i++;
+		}
+		return Math.sqrt(sum);
+	}
+
+	public void setCentroid(int index, double[] values) {
+		centroids[index] = values;
+	}
+
+	public double[] getCentroid(int index) {
+		return centroids[index];
+	}
+
+	public String toString() {
+		StringBuffer result = new StringBuffer(super.toString() + Tools.getLineSeparator());
+		result.append("Cluster centroids:" + Tools.getLineSeparator());
+		for (int i = 0; i < cm.getNumberOfClusters(); i++) {
+			Cluster cl = cm.getClusterAt(i);
+			result.append("Cluster " + cl.getId() + ":\t" + centroidToString(i) + Tools.getLineSeparator());
+		}
+		result.append(Tools.getLineSeparator());
+		for (int i = 0; i < this.centroids.length; i++) {
+			result.append("Centroid " + i + ":\t" + centroidToString(i) + "\t"+this.clustersizes[i]+Tools.getLineSeparator());
+		}
+		
+		result.append(Tools.getLineSeparators(2));
+		StringBuffer head = new StringBuffer();
+		head.append("id\t[");
+		for (int i = 0; i < centroids.length; i++) {
+			head.append("CL"+i);
+			if (i!=centroids.length-1) {
+				head.append(", ");
+			} else	{
+				head.append("]\n");
+			}
+		}
+		result.append(head);
+		for (int i = 0; i < es.size(); i++) {
+			result.append(i+"\t"+getMembership(es.getExample(i))+"\n");
+		}
+		
+		
+		return result.toString();
+	}
+
+	public String toResultString() {
+		return toString();
+	}
+
+	private String centroidToString(int index) {
+		StringBuffer s = new StringBuffer();
+		int i = 0;
+		for (Attribute att : es.getAttributes()) {
+			s.append(att.getName() + " = " + Tools.formatNumber(centroids[index][i++]) + " ");
+		}
+		return s.toString();
+	}
+}
